de68677e272ef8ff2277d42553c1029874ca8382
Yashaswini-200 <171708816+Yashaswini-200@users.noreply.github.com>
2026-06-03 11:03:03 +0000

    designed generate_temp()

--- src/temp.cpp
+++ src/temp.cpp
@@ -1,11 +1,9 @@
 #include "temp.h"
-enum TempState
-{
-    TEMP_NORMAL,
-    TEMP_HEATING,
-    TEMP_FAULT,
-    TEMP_RECOVERY
-};
+#include<stdlib.h>
 float generate_temperature(void){
-
+    float fluctuations = (((float)rand()/RAND_MAX)-0.5)*0.4;
+    const float target=37.0f;
+    static float temperature = 37.0f+fluctuations;
+    temperature+=0.1f*(target-temperature);
+    return temperature;
 }
