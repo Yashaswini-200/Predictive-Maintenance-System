c0867a0bfdfc6d3b4c66d6be613bdbd04091c11e
Yashaswini-200 <171708816+Yashaswini-200@users.noreply.github.com>
2026-06-04 06:36:26 +0000

    finished temp.cpp

--- src/temp.cpp
+++ src/temp.cpp
@@ -1,9 +1,10 @@
 #include "temp.h"
 #include<stdlib.h>
 float generate_temperature(void){
-    float fluctuations = (((float)rand()/RAND_MAX)-0.5)*0.4;
-    const float target=37.0f;
-    static float temperature = 37.0f+fluctuations;
-    temperature+=0.1f*(target-temperature);
+    float fluctuations = (((float)rand()/RAND_MAX)-0.5f)*0.4f;
+    const float target = 37.0f;
+    static float temperature = target;
+    temperature += fluctuations;
+    temperature += 0.1f*(target - temperature);
     return temperature;
 }
